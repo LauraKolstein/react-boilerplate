ef2c9c86334a27cc2dcee804cc9a87012f40860b
Laura Kolstein <ldkolstein@hotmail.com>
2017-03-14 13:05:39 -0400

    added id div to hmtl and error handing to gulpfile.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('It works!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Searchbar from './searchbar.js'
+import Header from './header.js'
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header />
+				<p>It's week 7!!!</p>
+				<p>The shortcuts don't work unless you press Control + E</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti culpa harum, voluptatem minima reprehenderit explicabo, optio, sequi non odio totam sed mollitia molestias. Consequuntur officiis sed error, beatae eos porro.</p>
+				<Searchbar />
+			</div>
+		)
+	}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
